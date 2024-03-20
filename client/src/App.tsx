2110848066ee7f7306e986c2ce1cfa8d21edc980
Luca Fischer <lucafischer@Lucas-Laptop.local>
2024-03-20 19:09:37 +0100

    Add calls to api in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,32 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
+  function callElevators() {
+    axios.get("http://localhost:3000/elevators").then((response) => {
+      const elevators = response.data;
+      const parsedElevators = JSON.stringify(elevators, null, 2);
+      console.log(parsedElevators);
+    } ).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  function callRoot() {
+    axios.get("http://localhost:3000").then((response) => {
+      console.log(response);
+    } ).catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
     <>
+    <button onClick={callRoot}>Call /</button>
+    <button onClick={callElevators}>Call /elevators</button>
       <button onClick={() => loginWithRedirect()}>Log In</button>;
       <button
         onClick={() =>
