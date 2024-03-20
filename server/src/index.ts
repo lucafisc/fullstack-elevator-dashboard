2fe3b2b0a8d06f85712f0e3c9a711c9d9c45f7cb
Luca Fischer <lucafischer@Lucas-Laptop.local>
2024-03-20 18:54:20 +0100

    Remove Auth0 from server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,6 @@ import express, {Express, Request, Response, NextFunction} from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { elevatorsRouter } from './routes/elevatorsRouter';
-import { auth, requiresAuth } from 'express-openid-connect';
 
 // Load environment variables
 dotenv.config();
@@ -10,15 +9,6 @@ const port = process.env.PORT || 3000;
 const mongoDB = process.env.DB_URL;
 const app : Express = express();
 
-// OpenID Connect configuration
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: `${process.env.BASE_URL}:${port}`,
-    clientID: process.env.CLIENT_ID,
-    issuerBaseURL: process.env.ISSUER_BASE_URL
-};
 
 // Connect to MongoDB
 mongoose.set('strictPopulate', false);
@@ -29,12 +19,12 @@ mongoose.connection.on('error', (error: Error) => console.error(error));
 
 // Middleware
 app.use(express.json());
-app.use(auth(config));
-app.use('/elevators', requiresAuth());
-app.use((req, res, next) => {
-    console.log('Request:', req.oidc.user);
-    next();
-  });
+// app.use(auth(config));
+// app.use('/elevators', requiresAuth());
+// app.use((req, res, next) => {
+//     console.log('Request:', req.oidc.user);
+//     next();
+//   });
 
 // Routes
 app.use('/', elevatorsRouter);
@@ -48,14 +38,14 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ message: err.message || 'Internal Server Error' });
 });
 
-app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
+// app.get('/', (req, res) => {
+//     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+//   });
 
-//   Add the requiresAuth middleware for routes that require authentication. Any route using this middleware will check for a valid user session and, if one does not exist, it will redirect the user to log in.
-  app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user));
-  });
+// //   Add the requiresAuth middleware for routes that require authentication. Any route using this middleware will check for a valid user session and, if one does not exist, it will redirect the user to log in.
+//   app.get('/profile', requiresAuth(), (req, res) => {
+//     res.send(JSON.stringify(req.oidc.user));
+//   });
 
 if (require.main === module) {
     app.listen(port, () => console.log(`Server is running on port ${port}`));
