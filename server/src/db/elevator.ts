f666544b351ed81b6226f19f1b9c06558e044866
Luca Fischer <lucafischer@Lucas-Laptop.local>
2024-03-21 11:27:12 +0100

    Add jwt token verification

--- server/src/db/elevator.ts
+++ server/src/db/elevator.ts
@@ -1,27 +1,25 @@
 import mongoose from "mongoose";
 import { Chart } from "./chart";
 export const elevatorTypesEnum = ["Passenger", "Freight"];
-export const stateEnum = ['operational', 'out-of-order', 'warning'];
-export type StateEnum = typeof stateEnum[number];
-
-
+export const stateEnum = ["operational", "out-of-order", "warning"];
+export type StateEnum = (typeof stateEnum)[number];
 
 const ElevatorSchema = new mongoose.Schema({
-    specifications: {
-        fabricationNumber: { type: String, required: true },
-        address: { type: String, required: true },
-        deviceIdentificationNumber: { type: String, required: true },
-        manufacturerName: { type: String, required: true },
-        productionYear: { type: Number, required: true },
-        elevatorType: { type: String, enum: elevatorTypesEnum, required: true },
-    },
-    operationalState: {
-        floorNumber: { type: Number, required: true },
-        state: { type: String, enum: stateEnum, required: true },
-        warningMessage: { type: String },
-        reason: { type: String },
-    },
-    chart: { type: mongoose.Schema.Types.ObjectId, ref: Chart }
+  specifications: {
+    fabricationNumber: { type: String, required: true },
+    address: { type: String, required: true },
+    deviceIdentificationNumber: { type: String, required: true },
+    manufacturerName: { type: String, required: true },
+    productionYear: { type: Number, required: true },
+    elevatorType: { type: String, enum: elevatorTypesEnum, required: true },
+  },
+  operationalState: {
+    floorNumber: { type: Number, required: true },
+    state: { type: String, enum: stateEnum, required: true },
+    warningMessage: { type: String },
+    reason: { type: String },
+  },
+  chart: { type: mongoose.Schema.Types.ObjectId, ref: Chart },
 });
 
-export const Elevator = mongoose.model('Elevator', ElevatorSchema);
+export const Elevator = mongoose.model("Elevator", ElevatorSchema);
