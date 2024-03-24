b3d7ba4674c08932b641aa0a88f213166439ef8f
Luca Fischer <lucafischer@lucas-laptop.speedport.ip>
2024-03-24 18:35:06 +0100

    Add Grid container tests

---
+++ client/src/components/ui/__tests__/GridContainer.test.tsx
@@ -0,0 +1,29 @@
+import { render } from "@testing-library/react";
+import GridContainer from "../GridContainer";
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/jest-globals";
+
+describe("GridContainer component", () => {
+  test("renders children", () => {
+    const { getByTestId } = render(
+      <GridContainer>
+        <div>Child 1</div>
+        <div>Child 2</div>
+      </GridContainer>
+    );
+
+    const gridContainerElement = getByTestId("grid-container");
+    expect(gridContainerElement).toBeInTheDocument();
+    expect(gridContainerElement.children).toHaveLength(2);
+  });
+
+  test("renders no children", () => {
+    const { getByTestId } = render(
+      <GridContainer children={undefined}></GridContainer>
+    );
+
+    const gridContainerElement = getByTestId("grid-container");
+    expect(gridContainerElement).toBeInTheDocument();
+    expect(gridContainerElement.children).toHaveLength(0);
+  });
+});
