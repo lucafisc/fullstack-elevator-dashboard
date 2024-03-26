ed48ebbd1b7cf9c38d6db425c50c79790e503120
Luca Fischer <lucafischer@lucas-laptop.speedport.ip>
2024-03-26 09:34:17 +0100

    Add footer

---
+++ client/src/components/pages/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <div className="pt-10 pb-4 mt-auto text-center">
+      <h1 className="mt-auto">Created by <a className="hover:text-accent" href="">Luca Fischer</a></h1>
+    </div>
+  );
+}
--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import ElevatorByState from "./ElevatorByState";
 import Header from "../ui/Header";
 import ElevatorById from "./ElevatorById";
+import Footer from "./Footer";
 
 type Props = {
   user: User;
@@ -12,13 +13,14 @@ type Props = {
 
 export default function Home({ user, logout }: Props) {
   return (
-    <div className="pb-10 sm:p-8 p-4">
+    <div className="flex flex-col justify-start h-full px-4 pt-4">
       <Header username={user.name} logout={logout} />
       <Routes>
         <Route path="/" element={<ElevatorOverview />} />
         <Route path="/state/:state" element={<ElevatorByState />} />
         <Route path="/elevator/:id" element={<ElevatorById />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
