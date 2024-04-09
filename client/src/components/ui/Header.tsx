62b57e04b23f532240221b729cdd822518c524c5
Luca Fischer <lucafischer@lucas-laptop.speedport.ip>
2024-04-09 09:04:15 +0200

    Add logoutParams to logout

--- client/src/components/ui/Header.tsx
+++ client/src/components/ui/Header.tsx
@@ -1,18 +1,19 @@
+import { LogoutOptions } from "@auth0/auth0-react";
 import Button from "./Button";
 
 type Props = {
   username: string | undefined;
-  logout: () => Promise<void>;
+  logout: (options?: LogoutOptions | undefined) => Promise<void>;
 };
 
 export default function Header({ username, logout }: Props) {
   return (
-    <header className="flex justify-between items-center mb-10">
-      <h1 className="sm:text-3xl text-xl">
+    <header className="flex items-center justify-between mb-10">
+      <h1 className="text-xl sm:text-3xl">
         {`Welcome, `}
         <span className="font-bold whitespace-nowrap">{username}</span>
       </h1>
-      <Button label={"Logout"} click={logout} />
+      <Button label={"Logout"} click={() => logout({ logoutParams: { returnTo: window.location.origin } })} />
     </header>
   );
 }
